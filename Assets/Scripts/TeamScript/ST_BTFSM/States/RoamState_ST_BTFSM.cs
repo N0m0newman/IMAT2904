737db598fd8e4630820c7a4cbe6edf8e06c2b4fa
Katsumi <102377066+Katsukyunn@users.noreply.github.com>
2022-04-01 01:30:08 +0100

    Add files via upload
    also crying

---
+++ Assets/Scripts/TeamScript/ST_BTFSM/States/RoamState_ST_BTFSM.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+public class RoamState_ST_BTFSM : BaseState_FSM_TS
+{
+
+    private SmartTank_ST_BTFSM AiTank_FSM;
+    public RoamState_ST_BTFSM(SmartTank_ST_BTFSM AiTank_FSM)
+
+    {
+
+        this.AiTank_FSM = AiTank_FSM;
+
+    }
+    public override Type StateEnter()
+    {
+        AiTank_FSM.targetTankPosition = null;
+        AiTank_FSM.consumablePosition = null;
+        AiTank_FSM.basePosition = null;
+        return null;
+
+    }
+    public override Type StateExit()
+
+    {
+
+        return null;
+
+    }
+
+    public override Type StateUpdate()
+    {
+            AiTank_FSM.EnemeyTankCheck();
+            AiTank_FSM.HealthCheck();
+            AiTank_FSM.AmmoCheck();
+            AiTank_FSM.FuelCheck();
+            AiTank_FSM.CollectableCheck();
+            AiTank_FSM.BaseFound();
+
+        if(AiTank_FSM.restockSequence.Evaluate() == BTNodeStates.SUCCESS)
+        { 
+
+            if (AiTank_FSM.FuelCheck() >= 60) //if the fuel is greater then or eqaul to 60 move at 0.5 speed
+            {
+                AiTank_FSM.RandomPath(0.5f);
+            }
+            if (AiTank_FSM.FuelCheck() < 60) //if the fuel is less then 60 move at 0.25 speed.
+            {
+                AiTank_FSM.RandomPath(0.25f);
+            }
+            if (AiTank_FSM.targetTanksFound.Count != 0 && AiTank_FSM.targetTanksFound.FirstOrDefault().Key != null) //If there is a target tank in range enter the chase.
+            {
+                Debug.Log("A");
+                return typeof(ChaseState_ST_BTFSM);
+            }
+            if (AiTank_FSM.consumablesFound.Count != 0 && AiTank_FSM.consumablesFound.FirstOrDefault().Key != null) //if there is consumables in range enter the collecatble.
+            {
+                Debug.Log("B");
+                return typeof(CollectState_ST_BTFSM);
+            }
+            if (AiTank_FSM.basesFound.Count != 0 && AiTank_FSM.consumablesFound.FirstOrDefault().Key != null) // if there is a enemy base fire at it.
+            {
+                Debug.Log("C");
+                AiTank_FSM.FireTank(AiTank_FSM.basePosition);
+            }
+            }
+        return null;
+
+        }
+
+
+
+}
+
